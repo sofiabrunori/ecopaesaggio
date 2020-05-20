47480ebcb69ec95ab28006d4ed7993ad61c8e678
sofiabrunori <62289023+sofiabrunori@users.noreply.github.com>
2020-05-20 13:06:52 +0200

    Update R_code_snowcover.r

--- R_code_snowcover.r
+++ R_code_snowcover.r
@@ -12,3 +12,29 @@ plot(snowmay,col=cl) #lo plotto
 #setto la nuova WD per la cartella dove ho messo le immagini .tif
 setwd("C:/lab_eco/snow")
 #ora tramite la funzione lapply importo tutto
+rlist=list.files(pattern=".tif", full.names=T)
+list_rast <- lapply(rlist, raster)
+snow.multitemp <- stack(list_rast)
+plot(snow.multitemp,col=cl)
+#facciamo dei confronti 2000 vs 2020
+par(mfrow=c(1,2))
+plot(snow.multitemp$snow2000r, col=cl)
+plot(snow.multitemp$snow2020r, col=cl)
+par(mfrow=c(2,1))
+#unifichiamo il limite tra le due immagini 
+
+plot(snow.multitemp$snow2000r, col=cl, zlim=c(0,250))
+plot(snow.multitemp$snow2020r, col=cl, zlim=c(0,250))
+
+#calcoliamoci la differenza
+difsnow = snow.multitemp$snow2020r - snow.multitemp$snow2000r
+ #diamo una nuova ramp palette
+cldiff <- colorRampPalette(c('blue','white','red'))(100) 
+plot(difsnow, col=cldiff)
+#facciamo una previsione multitemporale per vedere come saremo nel 2025 con la copertura nevosa
+#scarichiamo il file prediction nella cartella snow
+# per metterlo in r posso usare la funzione source
+    predicted.snow.2025.norm <- raster("predicted.snow.2025.norm.tif")
+    plot(predicted.snow.2025.norm, col=cl)
+ 
+
